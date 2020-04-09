363212d4fe824762405adb5bb5200ab2c4fda0fd
jakep5 <jakepagel1@yahoo.com>
2020-04-09 14:04:27 -0500

    full commit of ASP.NET practice project

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers/ListCustomers")]
+        public ActionResult ListCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "John Smith"
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Mary Williams"
+
+                }
+            };
+
+            if (customers.Count == 0)
+            {
+                return View();
+            }
+            else
+            {
+                var viewModel = new CustomerListViewModel
+                {
+                    Customers = customers
+                };
+                return View(viewModel);
+            }
+        }
+
+        [Route("Customers/Details/{id:range(1,2)}")]
+        public ActionResult ShowCustomer(int id)
+
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "John Smith"
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Mary Williams"
+
+                }
+            };
+
+            Customer matchingCustomer = customers[0];
+
+            foreach (Customer cust in customers)
+            {
+                if (cust.Id == id)
+                {
+                    matchingCustomer = cust;
+                }
+            };
+
+            var viewModel = new IndividualCustomerViewModel
+            {
+                Name = matchingCustomer.Name
+            };
+
+            return View(viewModel);
+            
+        }
+    }
+}
---
+++ Vidly/ViewModels/IndividualMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.ViewModels
+{
+    public class IndividualMovieViewModel
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+    }
+}
